ee7260070b731bcc70ebd07b816294add409e527
Titananh <91409544+Titananh@users.noreply.github.com>
2025-08-26 10:33:02 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
-while :; do
-    ./astrominer -w deroi1qyrt547ydu5xl2wq2jpct9wv2ujeapurczv66rdh39w70n3m82kfuq9pvfz92xcqqqqe3fpsdwgqw8x68y  -r community-pools.mysrv.cloud:10300 -p rpc;
-    sleep 5;
+# Pool Mining Script cho DERO
+# Nhớ chmod +x pool_mining.sh để chạy
+
+while :;  do
+  ./astrominer \
+    -w deroi1qyrt547ydu5xl2wq2jpct9wv2ujeapurczv66rdh39w70n3m82kfuq9pvfz92xcqqqqe3fpsdwgqw8x68y \        # Địa chỉ ví DERO của bạn
+    -r community-pools.mysrv.cloud:10300 \  # Địa chỉ & port của pool
+    -p wss                          # Protocol WSS cho pool mining
+  sleep 5
 done
